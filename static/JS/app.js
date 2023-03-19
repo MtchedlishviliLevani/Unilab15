07dd99b5b61752ab398622eb08b6e85b1ae09865
MtchedlishviliLevani <levan.mtchedlishvili.5@iliauni.edu.ge>
2023-03-19 23:37:26 +0400

    First commit

---
+++ static/JS/app.js
@@ -0,0 +1,40 @@
+const burger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".menu");
+
+burger.addEventListener("click", () =>  {
+  burger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+});
+
+document.querySelectorAll("nav-link").forEach(n =>n. addEventListener("click", () => {
+    burger.classList.remove("active")
+    navMenu.classList.remove("active")
+  }))
+
+
+// margintop for main when i click burger menu
+
+  const main = document.querySelector("main");
+  const hamburger = document.querySelector(".hamburger");
+
+  hamburger.addEventListener("click", () => {
+ main.style.margin = "40rem 0 0 0";
+  
+  })
+
+
+  // Modal box
+
+  const button = document.querySelector("#btn");
+const close_cl = document.querySelector(".close");
+const windowsA = document.querySelector(".window");
+
+button.addEventListener("click", () => {
+
+windowsA.style.display = "block";
+
+})
+
+close_cl.addEventListener("click",  () => {
+    windowsA.style.display = "none"
+})
